d924d4ed694cc60b94261904453415fd501985e8
Sean Wong <seanwong24@live.com>
2023-10-15 12:36:46 +0000

    update deps

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { createComponent } from '@lit-labs/react';
+import { createComponent } from '@lit/react';
 import { AwesomeMarkdownElement } from "@awesome-elements/markdown";
 
 export const Markdown = createComponent({
